df6c18d37c699626b317852705b6d3697550a3bd
Xzavier0722 <Xzavier0722@gmail.com>
2022-04-18 11:12:22 +0800

    fix(module): fix protection check for KingdomX module

--- src/main/java/com/xzavier0722/mc/plugin/slimeglue/module/KingdomsXModule.java
+++ src/main/java/com/xzavier0722/mc/plugin/slimeglue/module/KingdomsXModule.java
@@ -2,11 +2,13 @@ package com.xzavier0722.mc.plugin.slimeglue.module;
 
 import com.xzavier0722.mc.plugin.slimeglue.api.ACompatibilityModule;
 import com.xzavier0722.mc.plugin.slimeglue.api.protection.IBlockProtectionHandler;
+import com.xzavier0722.mc.plugin.slimeglue.api.protection.IPlayerProtectionHandler;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.Plugin;
 import org.kingdoms.constants.group.Kingdom;
-import org.kingdoms.constants.player.KingdomPlayer;
+import org.kingdoms.constants.land.Land;
+import org.kingdoms.constants.land.location.SimpleChunkLocation;
 
 public class KingdomsXModule extends ACompatibilityModule {
 
@@ -14,27 +16,46 @@ public class KingdomsXModule extends ACompatibilityModule {
         addProtectionHandler(new IBlockProtectionHandler() {
             @Override
             public boolean canPlaceBlock(OfflinePlayer player, Location location) {
-                return isClaimed(player, location);
+                return canAccess(player, location);
             }
 
             @Override
             public boolean canBreakBlock(OfflinePlayer player, Location location) {
-                return isClaimed(player, location);
+                return canAccess(player, location);
             }
 
             @Override
             public boolean canInteractBlock(OfflinePlayer player, Location location) {
-                return isClaimed(player, location);
-            }
-
-            private boolean isClaimed(OfflinePlayer p, Location l) {
-                Kingdom kingdom = KingdomPlayer.getKingdomPlayer(p).getKingdom();
-                if (kingdom == null) {
-                    return true;
-                }
-                return kingdom.isClaimed(l);
+                return canAccess(player, location);
             }
         });
+
+        addProtectionHandler(new IPlayerProtectionHandler() {
+            @Override
+            public boolean canAttackPlayer(OfflinePlayer player, Location location) {
+                return canAccess(player, location);
+            }
+
+            @Override
+            public boolean canAttackEntity(OfflinePlayer player, Location location) {
+                return canAccess(player, location);
+            }
+
+            @Override
+            public boolean canInteractEntity(OfflinePlayer player, Location location) {
+                return canAccess(player, location);
+            }
+        });
+    }
+
+    private boolean canAccess(OfflinePlayer p, Location l) {
+        Land land = SimpleChunkLocation.of(l).getLand();
+        if (land == null || !land.isClaimed()) {
+            return true;
+        }
+
+        Kingdom kingdom = land.getKingdom();
+        return kingdom == null || p.getUniqueId().equals(kingdom.getKingId()) || kingdom.isMember(p);
     }
 
     @Override
