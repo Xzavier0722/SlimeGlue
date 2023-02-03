3cc0e9f1305767a7bbc06424910221252b85e866
Xzavier0722 <xzavier0722@gmail.com>
2023-02-03 12:45:46 +0800

    Opt performance

--- src/main/java/com/xzavier0722/mc/plugin/slimeglue/slimefun/GlueProtectionModule.java
+++ src/main/java/com/xzavier0722/mc/plugin/slimeglue/slimefun/GlueProtectionModule.java
@@ -3,7 +3,6 @@ package com.xzavier0722.mc.plugin.slimeglue.slimefun;
 import com.xzavier0722.mc.plugin.slimeglue.SlimeGlue;
 import com.xzavier0722.mc.plugin.slimeglue.api.protection.IBlockProtectionHandler;
 import com.xzavier0722.mc.plugin.slimeglue.api.protection.IPlayerProtectionHandler;
-import com.xzavier0722.mc.plugin.slimeglue.api.protection.IProtectionHandler;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.ProtectionModule;
 import org.bukkit.Location;
@@ -26,72 +25,72 @@ public class GlueProtectionModule implements ProtectionModule {
     public boolean hasPermission(OfflinePlayer p, Location l, Interaction interaction) {
         switch (interaction) {
             case BREAK_BLOCK:
-                for (IProtectionHandler each : SlimeGlue.moduleManager().getProtectionHandlers()) {
-                    if (each.bypassCheck(p, l)) {
-                        continue;
-                    }
-                    if (each instanceof IBlockProtectionHandler) {
-                        if (!((IBlockProtectionHandler) each).canBreakBlock(p, l)) {
+                for (var each : SlimeGlue.moduleManager().getProtectionHandlers()) {
+                    if (each instanceof IBlockProtectionHandler handler) {
+                        if (handler.bypassCheck(p, l)) {
+                            continue;
+                        }
+                        if (!handler.canBreakBlock(p, l)) {
                             return false;
                         }
                     }
                 }
                 break;
             case PLACE_BLOCK:
-                for (IProtectionHandler each : SlimeGlue.moduleManager().getProtectionHandlers()) {
-                    if (each.bypassCheck(p, l)) {
-                        continue;
-                    }
-                    if (each instanceof IBlockProtectionHandler) {
-                        if (!((IBlockProtectionHandler) each).canPlaceBlock(p, l)) {
+                for (var each : SlimeGlue.moduleManager().getProtectionHandlers()) {
+                    if (each instanceof IBlockProtectionHandler handler) {
+                        if (handler.bypassCheck(p, l)) {
+                            continue;
+                        }
+                        if (!handler.canPlaceBlock(p, l)) {
                             return false;
                         }
                     }
                 }
                 break;
             case ATTACK_ENTITY:
-                for (IProtectionHandler each : SlimeGlue.moduleManager().getProtectionHandlers()) {
-                    if (each.bypassCheck(p, l)) {
-                        continue;
-                    }
-                    if (each instanceof IPlayerProtectionHandler) {
-                        if (!((IPlayerProtectionHandler) each).canAttackEntity(p, l)) {
+                for (var each : SlimeGlue.moduleManager().getProtectionHandlers()) {
+                    if (each instanceof IPlayerProtectionHandler handler) {
+                        if (handler.bypassCheck(p, l)) {
+                            continue;
+                        }
+                        if (!handler.canAttackEntity(p, l)) {
                             return false;
                         }
                     }
                 }
                 break;
             case ATTACK_PLAYER:
-                for (IProtectionHandler each : SlimeGlue.moduleManager().getProtectionHandlers()) {
-                    if (each.bypassCheck(p, l)) {
-                        continue;
-                    }
-                    if (each instanceof IPlayerProtectionHandler) {
-                        if (!((IPlayerProtectionHandler) each).canAttackPlayer(p, l)) {
+                for (var each : SlimeGlue.moduleManager().getProtectionHandlers()) {
+                    if (each instanceof IPlayerProtectionHandler handler) {
+                        if (handler.bypassCheck(p, l)) {
+                            continue;
+                        }
+                        if (!handler.canAttackPlayer(p, l)) {
                             return false;
                         }
                     }
                 }
                 break;
             case INTERACT_BLOCK:
-                for (IProtectionHandler each : SlimeGlue.moduleManager().getProtectionHandlers()) {
-                    if (each.bypassCheck(p, l)) {
-                        continue;
-                    }
-                    if (each instanceof IBlockProtectionHandler) {
-                        if (!((IBlockProtectionHandler) each).canInteractBlock(p, l)) {
+                for (var each : SlimeGlue.moduleManager().getProtectionHandlers()) {
+                    if (each instanceof IBlockProtectionHandler handler) {
+                        if (handler.bypassCheck(p, l)) {
+                            continue;
+                        }
+                        if (!handler.canInteractBlock(p, l)) {
                             return false;
                         }
                     }
                 }
                 break;
             case INTERACT_ENTITY:
-                for (IProtectionHandler each : SlimeGlue.moduleManager().getProtectionHandlers()) {
-                    if (each.bypassCheck(p, l)) {
-                        continue;
-                    }
-                    if (each instanceof IPlayerProtectionHandler) {
-                        if (!((IPlayerProtectionHandler) each).canInteractEntity(p, l)) {
+                for (var each : SlimeGlue.moduleManager().getProtectionHandlers()) {
+                    if (each instanceof IPlayerProtectionHandler handler) {
+                        if (handler.bypassCheck(p, l)) {
+                            continue;
+                        }
+                        if (!handler.canInteractEntity(p, l)) {
                             return false;
                         }
                     }
